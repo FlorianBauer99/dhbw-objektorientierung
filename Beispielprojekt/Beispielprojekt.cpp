1aa1edc6984278ad96a0fd9d3a5d41dee28d4ac5
FlorianBauer99 <Bauer.Florian99@gmx.de>
2018-11-06 18:50:58 +0100

    Fadenkreuz bleibt im Screen Kommentare ergänzt

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -8,14 +8,16 @@
 using namespace std;
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
+const int Breite_Screen = 1600;
+const int Hoehe_screen = 900;
 
 class GameWindow : public Gosu::Window
 {
 public:
 	Gosu::Image fadenkreuz, florian, gandhi, herz, hintergrund, hoecke, kfc, obama, patrone, patronenkiste, putin, seehofer, trump;
 	GameWindow()
-		: Window(1600, 900)
-		,fadenkreuz("Fadenkreuz.png")
+		: Window(Breite_Screen, Hoehe_screen) //initialisiert das Spielfenster mit Breite*Hoehe
+		,fadenkreuz("Fadenkreuz.png")//läd alle Bilder aus dem Speicherordner
 		,florian("Florian.png")
 		,gandhi ("Gandhi.png")
 		,herz("Herz.png")
@@ -30,36 +32,60 @@ public:
 		,trump("Trump.png")
 		
 	{
-		set_caption("Promi Moorhuhn");
+		set_caption("Promi Moorhuhn"); //ändert den Title des geöffneten Spielfensters
 
 	}
+	//definition benötigter Variabeln
+
+	//sonstiges
 	int i = 0;
+	//Mausposition
 	double mouse_x = 0;
 	double mouse_y = 0;
+	//Position verschiedenener Bilder
+	double fadenkreuz_x = 0;
+	double fadenkreuz_y = 0;
 	
 	void draw() override
 	{
-		
+		//Zeichnet die verschiedenen Bilder Bild.draw_rot(xpos, ypos, zpos, Drehwinkel in Bogenmaß, pos der Mitte(breite) im Bild, pos der Mitte (hoehe) im Bild
 		hintergrund.draw_rot(800, 450, 0.0, 0.0,
 			0.5, 0.5
 		);
-			hoecke.draw_rot(800, 450, 0.0, 0.0,
-				0.5, 0.5
-			);
-			fadenkreuz.draw_rot(mouse_x, mouse_y, 0.0, 0.0,
-				0.5, 0.5
-			);
+		hoecke.draw_rot(800, 450, 0.0, 0.0,
+			0.5, 0.5
+		);
+		fadenkreuz.draw_rot(fadenkreuz_x, fadenkreuz_y, 0.0, 0.0,
+			0.5, 0.5
+		);
 	}
 	
 	
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
+		//Vorübungen, nicht nötig, nur als Info
 		i = (i + 3) % 800;
 		mouse_x = input().mouse_x();
 		mouse_y = input().mouse_y();
 		if (input().down(Gosu::ButtonName::KB_W)) {
 			mouse_x = mouse_x + mouse_y;
+		}
+
+		//Fadenkreuz bleibt in bestimmtem Bereich, aktuell genau im Screen, später eventuell freier bereich am unteren Rand für Spielinfos?
+		fadenkreuz_x = mouse_x;
+		if (fadenkreuz_x < 0) {
+			fadenkreuz_x = 0;
+		}
+		if (fadenkreuz_x > Breite_Screen) {
+			fadenkreuz_x = Breite_Screen;
+		}
+		fadenkreuz_y = mouse_y;
+		if (fadenkreuz_y < 0) {
+			fadenkreuz_y = 0;
+		}
+		if (fadenkreuz_y > Hoehe_screen) {
+			fadenkreuz_y = Hoehe_screen;
 		}
 	}
 };
@@ -67,10 +93,7 @@ public:
 // C++ Hauptprogramm
 int main()
 {
-	int a=5;
 	GameWindow window;
 	window.show();
-	
-
 }
 
